0bbc7630f21d3f7820cec532cab2b5b671cf9423
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-03-23 21:57:58 -0700

    tutorial watching + planning

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const day = Date().toString().split(" ");
 console.log(day[2]);
 
 let cell = document.querySelectorAll('th')
-cell.forEach((cl, i) => {
-    
-})
+
+// on click of each cell, get the corresponding to-do list for that day.
+// need button for create task, remove task, edit task. also need checkbox
+// if all tasks for that day were checked, mark that day with a green box, red if not
+// need the correct number of dates for the month
+// implement localstorage
