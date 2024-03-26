28acebd409f163e0f2868db6f0fa2478a57cf0ac
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-03-25 23:16:26 -0700

    buttons

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ console.log(day[2]);
 
 let cell = document.querySelectorAll('th')
 
+let createTask = document.createElement('li')
+let node
+
 // on click of each cell, get the corresponding to-do list for that day.
 // need button for create task, remove task, edit task. also need checkbox
 // if all tasks for that day were checked, mark that day with a green box, red if not
