db06581fd81d471c7dfc8234157f87982a3cd9bf
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-03-31 22:36:48 -0700

    added hide/unhide function

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-const date = new Date()
-const day = Date().toString().split(" ");
-console.log(day[2]);
+const datePicker = document.querySelector('.datepicker');
+const dateInput = document.querySelector('.date-input');
+const cancelBtn = document.querySelector('.cancel');
+const applyBtn = document.querySelector('.apply');
+const dates = document.querySelector(".dates");
 
-let cell = document.querySelectorAll('th')
 
-let createTask = document.createElement('li')
-let datePicker = document.getElementById('datepicker')
-let datePickerHeader = document.getElementById('datepicker-header')
+dateInput.addEventListener('click', () => {
+    datePicker.hidden = false;
+})
 
+cancelBtn.addEventListener('click', () => {
+    datePicker.hidden = false;
+})
+
+applyBtn.addEventListener('click', () => {
+    datePicker.hidden = false;
+})
 // on click of each cell, get the corresponding to-do list for that day.
 // need button for create task, remove task, edit task. also need checkbox
 // if all tasks for that day were checked, mark that day with a green box, red if not
