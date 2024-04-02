55fc15ca10fbbfe01dc9f2ed6473b88c903ba28e
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-04-01 23:22:20 -0700

    year value config

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const datePicker = document.querySelector('.datepicker');
 const dateInput = document.querySelector('.date-input');
 const cancelBtn = document.querySelector('.cancel');
 const applyBtn = document.querySelector('.apply');
-const dates = document.querySelector(".dates");
-
+const dates = document.querySelector('.dates');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const yearInput = document.querySelector('.year-input');
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
 
 dateInput.addEventListener('click', () => {
     datePicker.hidden = false;
@@ -16,6 +21,30 @@ cancelBtn.addEventListener('click', () => {
 applyBtn.addEventListener('click', () => {
     datePicker.hidden = false;
 })
+
+prev.addEventListener('click', () => {
+    yearInput.value = parseInt(yearInput.value) - 1;
+})
+
+next.addEventListener('click', () => {
+    yearInput.value = parseInt(yearInput.value) + 1;
+})
+
+const displayDates = () => {
+    dates.innerHTML = "";
+    const lastOfMonth = new Date(year, month + 1, 0);
+}
+
+const createButton = (text, isDisabled, isToday) => {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.disabled = isDisabled;
+    button.classList.toggle('today', isToday);
+    return button;
+}
+
+displayDates();
+
 // on click of each cell, get the corresponding to-do list for that day.
 // need button for create task, remove task, edit task. also need checkbox
 // if all tasks for that day were checked, mark that day with a green box, red if not
